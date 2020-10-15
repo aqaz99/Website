75d4e2e431aedad057b31c94d4953cbf88549e1d
James <jminerswebb@gmail.com>
2020-10-14 17:54:11 -0700

    aws rds

--- addEntity.php
+++ addEntity.php
@@ -1,5 +1,5 @@
 <?php
-    $db = pg_connect("host=localhost port=5574 dbname=erasmus user=postgres password=aqazaqaz");
+    $db = pg_connect("host=localhost port=5432 dbname=postgres user=kronos password=aqazaqaz");
     $query = "INSERT INTO Underling VALUES ('$_POST[name]','$_POST[race]',
     '$_POST[gender]','$_POST[age]','$_POST[inventory]',
     '$_POST[current_op]')";
